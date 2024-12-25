2d08c85afa12a70a9c516c708236807214f8ee46
Krakozaybr <krakozaybp@gmail.com>
2024-12-25 23:34:31 +0300

    Added comments to script.bash. Created README.md

--- script.bash
+++ script.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Получаем папку, содержащую файл скрипта
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# С помощью curl получаем данные о биткоине
 JSON=$( curl -s https://api.coincap.io/v2/assets/bitcoin )
+
+# С помощью jq парсим json и получаем нужное цену и timestamp
 PRICE=$( echo "${JSON}" | jq -r ".data.priceUsd" )
 TIMESTAMP=$( echo "${JSON}" | jq -r ".timestamp" )
-echo "${JSON}"
+
+# Пишем данные в текстовый файл
 echo "${PRICE}" >> "${SCRIPT_DIR}/Bitcoin_${TIMESTAMP}.txt"
